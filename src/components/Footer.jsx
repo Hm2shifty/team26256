0d2c63a9ac672472467440df99e057cf8d8de32d
hethm1heswari-pixel <heth.m1heswari@gmail.com>
2026-05-11 13:14:02 -0400

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <p>&copy; 2026 SWERVO FTC 26256. All systems operational.</p>
         <div className="footer-links">
           <a href="https://firstinspires.org" target="_blank" rel="noopener noreferrer">FIRST</a>
-          <a href="https://github.com/Hm2shifty" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://github.com/SWERVO-26256" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
       </div>
     </footer>
